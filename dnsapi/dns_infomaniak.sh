13690e646d2bb3e7d15f0136b4ef9aa0f6bc749f
jdn <jean-francois.daguin.external@atos.net>
2025-06-26 14:41:23 +0200

    shfmt

--- dnsapi/dns_infomaniak.sh
+++ dnsapi/dns_infomaniak.sh
@@ -26,147 +26,147 @@ DEFAULT_INFOMANIAK_TTL=300
 #Usage: dns_infomaniak_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_infomaniak_add() {
 
-  INFOMANIAK_API_TOKEN="${INFOMANIAK_API_TOKEN:-$(_readaccountconf_mutable INFOMANIAK_API_TOKEN)}"
-  INFOMANIAK_API_URL="${INFOMANIAK_API_URL:-$(_readaccountconf_mutable INFOMANIAK_API_URL)}"
-  INFOMANIAK_TTL="${INFOMANIAK_TTL:-$(_readaccountconf_mutable INFOMANIAK_TTL)}"
+	INFOMANIAK_API_TOKEN="${INFOMANIAK_API_TOKEN:-$(_readaccountconf_mutable INFOMANIAK_API_TOKEN)}"
+	INFOMANIAK_API_URL="${INFOMANIAK_API_URL:-$(_readaccountconf_mutable INFOMANIAK_API_URL)}"
+	INFOMANIAK_TTL="${INFOMANIAK_TTL:-$(_readaccountconf_mutable INFOMANIAK_TTL)}"
 
-  if [ -z "$INFOMANIAK_API_TOKEN" ]; then
-    INFOMANIAK_API_TOKEN=""
-    _err "Please provide a valid Infomaniak API token in variable INFOMANIAK_API_TOKEN"
-    return 1
-  fi
+	if [ -z "$INFOMANIAK_API_TOKEN" ]; then
+		INFOMANIAK_API_TOKEN=""
+		_err "Please provide a valid Infomaniak API token in variable INFOMANIAK_API_TOKEN"
+		return 1
+	fi
 
-  if [ -z "$INFOMANIAK_API_URL" ]; then
-    INFOMANIAK_API_URL="$DEFAULT_INFOMANIAK_API_URL"
-  fi
+	if [ -z "$INFOMANIAK_API_URL" ]; then
+		INFOMANIAK_API_URL="$DEFAULT_INFOMANIAK_API_URL"
+	fi
 
-  if [ -z "$INFOMANIAK_TTL" ]; then
-    INFOMANIAK_TTL="$DEFAULT_INFOMANIAK_TTL"
-  fi
+	if [ -z "$INFOMANIAK_TTL" ]; then
+		INFOMANIAK_TTL="$DEFAULT_INFOMANIAK_TTL"
+	fi
 
-  #save the token to the account conf file.
-  _saveaccountconf_mutable INFOMANIAK_API_TOKEN "$INFOMANIAK_API_TOKEN"
+	#save the token to the account conf file.
+	_saveaccountconf_mutable INFOMANIAK_API_TOKEN "$INFOMANIAK_API_TOKEN"
 
-  if [ "$INFOMANIAK_API_URL" != "$DEFAULT_INFOMANIAK_API_URL" ]; then
-    _saveaccountconf_mutable INFOMANIAK_API_URL "$INFOMANIAK_API_URL"
-  fi
+	if [ "$INFOMANIAK_API_URL" != "$DEFAULT_INFOMANIAK_API_URL" ]; then
+		_saveaccountconf_mutable INFOMANIAK_API_URL "$INFOMANIAK_API_URL"
+	fi
 
-  if [ "$INFOMANIAK_TTL" != "$DEFAULT_INFOMANIAK_TTL" ]; then
-    _saveaccountconf_mutable INFOMANIAK_TTL "$INFOMANIAK_TTL"
-  fi
+	if [ "$INFOMANIAK_TTL" != "$DEFAULT_INFOMANIAK_TTL" ]; then
+		_saveaccountconf_mutable INFOMANIAK_TTL "$INFOMANIAK_TTL"
+	fi
 
-  export _H1="Authorization: Bearer $INFOMANIAK_API_TOKEN"
-  export _H2="Content-Type: application/json"
+	export _H1="Authorization: Bearer $INFOMANIAK_API_TOKEN"
+	export _H2="Content-Type: application/json"
 
-  fulldomain="$1"
-  txtvalue="$2"
+	fulldomain="$1"
+	txtvalue="$2"
 
-  _info "Infomaniak DNS API"
-  _debug fulldomain "$fulldomain"
-  _debug txtvalue "$txtvalue"
+	_info "Infomaniak DNS API"
+	_debug fulldomain "$fulldomain"
+	_debug txtvalue "$txtvalue"
 
-  # guess which base domain to add record to
-  zone=$(_get_zone "$fulldomain")
-  if [ -z "$zone" ]; then
-    _err "cannot find zone:<${zone}> to modify"
-    return 1
-  fi
+	# guess which base domain to add record to
+	zone=$(_get_zone "$fulldomain")
+	if [ -z "$zone" ]; then
+		_err "cannot find zone:<${zone}> to modify"
+		return 1
+	fi
 
-  _debug "zone:$zone"
+	_debug "zone:$zone"
 
-  # payload
-  data="{\"type\": \"TXT\", \"source\": \"$zone\", \"target\": \"$txtvalue\", \"ttl\": $INFOMANIAK_TTL}"
+	# payload
+	data="{\"type\": \"TXT\", \"source\": \"$zone\", \"target\": \"$txtvalue\", \"ttl\": $INFOMANIAK_TTL}"
 
-  # API call
-  response=$(_post "$data" "${INFOMANIAK_API_URL}/2/zones/${zone}/records")
-  if [ -n "$response" ] && echo "$response" | _contains '"result":"success"'; then
-    _info "Record added"
-    _debug "Response: $response"
-    return 0
-  fi
-  _err "could not create record"
-  _debug "Response: $response"
-  return 1
+	# API call
+	response=$(_post "$data" "${INFOMANIAK_API_URL}/2/zones/${zone}/records")
+	if [ -n "$response" ] && echo "$response" | _contains '"result":"success"'; then
+		_info "Record added"
+		_debug "Response: $response"
+		return 0
+	fi
+	_err "could not create record"
+	_debug "Response: $response"
+	return 1
 }
 
 #Usage: fulldomain txtvalue
 #Remove the txt record after validation.
 dns_infomaniak_rm() {
 
-  INFOMANIAK_API_TOKEN="${INFOMANIAK_API_TOKEN:-$(_readaccountconf_mutable INFOMANIAK_API_TOKEN)}"
-  INFOMANIAK_API_URL="${INFOMANIAK_API_URL:-$(_readaccountconf_mutable INFOMANIAK_API_URL)}"
-  INFOMANIAK_TTL="${INFOMANIAK_TTL:-$(_readaccountconf_mutable INFOMANIAK_TTL)}"
+	INFOMANIAK_API_TOKEN="${INFOMANIAK_API_TOKEN:-$(_readaccountconf_mutable INFOMANIAK_API_TOKEN)}"
+	INFOMANIAK_API_URL="${INFOMANIAK_API_URL:-$(_readaccountconf_mutable INFOMANIAK_API_URL)}"
+	INFOMANIAK_TTL="${INFOMANIAK_TTL:-$(_readaccountconf_mutable INFOMANIAK_TTL)}"
 
-  if [ -z "$INFOMANIAK_API_TOKEN" ]; then
-    INFOMANIAK_API_TOKEN=""
-    _err "Please provide a valid Infomaniak API token in variable INFOMANIAK_API_TOKEN"
-    return 1
-  fi
+	if [ -z "$INFOMANIAK_API_TOKEN" ]; then
+		INFOMANIAK_API_TOKEN=""
+		_err "Please provide a valid Infomaniak API token in variable INFOMANIAK_API_TOKEN"
+		return 1
+	fi
 
-  if [ -z "$INFOMANIAK_API_URL" ]; then
-    INFOMANIAK_API_URL="$DEFAULT_INFOMANIAK_API_URL"
-  fi
+	if [ -z "$INFOMANIAK_API_URL" ]; then
+		INFOMANIAK_API_URL="$DEFAULT_INFOMANIAK_API_URL"
+	fi
 
-  if [ -z "$INFOMANIAK_TTL" ]; then
-    INFOMANIAK_TTL="$DEFAULT_INFOMANIAK_TTL"
-  fi
+	if [ -z "$INFOMANIAK_TTL" ]; then
+		INFOMANIAK_TTL="$DEFAULT_INFOMANIAK_TTL"
+	fi
 
-  #save the token to the account conf file.
-  _saveaccountconf_mutable INFOMANIAK_API_TOKEN "$INFOMANIAK_API_TOKEN"
+	#save the token to the account conf file.
+	_saveaccountconf_mutable INFOMANIAK_API_TOKEN "$INFOMANIAK_API_TOKEN"
 
-  if [ "$INFOMANIAK_API_URL" != "$DEFAULT_INFOMANIAK_API_URL" ]; then
-    _saveaccountconf_mutable INFOMANIAK_API_URL "$INFOMANIAK_API_URL"
-  fi
+	if [ "$INFOMANIAK_API_URL" != "$DEFAULT_INFOMANIAK_API_URL" ]; then
+		_saveaccountconf_mutable INFOMANIAK_API_URL "$INFOMANIAK_API_URL"
+	fi
 
-  if [ "$INFOMANIAK_TTL" != "$DEFAULT_INFOMANIAK_TTL" ]; then
-    _saveaccountconf_mutable INFOMANIAK_TTL "$INFOMANIAK_TTL"
-  fi
+	if [ "$INFOMANIAK_TTL" != "$DEFAULT_INFOMANIAK_TTL" ]; then
+		_saveaccountconf_mutable INFOMANIAK_TTL "$INFOMANIAK_TTL"
+	fi
 
-  export _H1="Authorization: Bearer $INFOMANIAK_API_TOKEN"
-  export _H2="ContentType: application/json"
+	export _H1="Authorization: Bearer $INFOMANIAK_API_TOKEN"
+	export _H2="ContentType: application/json"
 
-  fulldomain=$1
-  txtvalue=$2
-  _info "Infomaniak DNS API"
-  _debug fulldomain "$fulldomain"
-  _debug txtvalue "$txtvalue"
+	fulldomain=$1
+	txtvalue=$2
+	_info "Infomaniak DNS API"
+	_debug fulldomain "$fulldomain"
+	_debug txtvalue "$txtvalue"
 
-  # guess which base domain to add record to
-  zone=$(_get_zone "$fulldomain")
-  if [ -z "$zone" ]; then
-    _err "cannot find zone:<$zone> to modify"
-    return 1
-  fi
+	# guess which base domain to add record to
+	zone=$(_get_zone "$fulldomain")
+	if [ -z "$zone" ]; then
+		_err "cannot find zone:<$zone> to modify"
+		return 1
+	fi
 
-  _debug "zone:$zone"
+	_debug "zone:$zone"
 
-  # find previous record
-  # shellcheck disable=SC2086
-  record_id=$(_get "${INFOMANIAK_API_URL}/2/zones/${zone}/records" |\
-        sed 's/.*"data":\[\(.*\)\]}/\1/; s/},{/}{/g' |\
-       	sed -n 's/.*"id":"*\([0-9]*\)"*.*"source":"'$zone'".*"target":"\\"'$txtvalue'\\"".*/\1/p')
-  if [ -z "$record_id" ]; then
-    _err "could not find record to delete"
-    return 1
-  fi
-  _debug "record_id: $record_id"
+	# find previous record
+	# shellcheck disable=SC2086
+	record_id=$(_get "${INFOMANIAK_API_URL}/2/zones/${zone}/records" |
+		sed 's/.*"data":\[\(.*\)\]}/\1/; s/},{/}{/g' |
+		sed -n 's/.*"id":"*\([0-9]*\)"*.*"source":"'$zone'".*"target":"\\"'$txtvalue'\\"".*/\1/p')
+	if [ -z "$record_id" ]; then
+		_err "could not find record to delete"
+		return 1
+	fi
+	_debug "record_id: $record_id"
 
-  # API call
-  response=$(_post "" "${INFOMANIAK_API_URL}/2/zones/${zone}/records/${record_id}" "" DELETE)
-  if [ -n "$response" ] && echo "$response" | _contains '"result":"success"'; then
-    _info "Record deleted"
-    return 0
-  fi
-  _err "could not delete record"
-  return 1
+	# API call
+	response=$(_post "" "${INFOMANIAK_API_URL}/2/zones/${zone}/records/${record_id}" "" DELETE)
+	if [ -n "$response" ] && echo "$response" | _contains '"result":"success"'; then
+		_info "Record deleted"
+		return 0
+	fi
+	_err "could not delete record"
+	return 1
 }
 
 ####################  Private functions below ##################################
 
 _get_zone() {
-  domain="$1"
-  # Whatever the domain is, you can get the fqdn with the following.
-  # shellcheck disable=SC1004
-  response=$(_get "${INFOMANIAK_API_URL}/2/domains/${domain}/zones" | sed 's/.*\[{"fqdn"\:"\(.*\)/\1/')
-  echo "${response%%\"*}"
+	domain="$1"
+	# Whatever the domain is, you can get the fqdn with the following.
+	# shellcheck disable=SC1004
+	response=$(_get "${INFOMANIAK_API_URL}/2/domains/${domain}/zones" | sed 's/.*\[{"fqdn"\:"\(.*\)/\1/')
+	echo "${response%%\"*}"
 }
